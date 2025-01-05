cb62305c08a24c552045d898eaf3ade32e7b230f
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2025-01-06 03:04:29 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden' id='Footer'>
       <div className='container mx-auto flex flex-col md:flex-row justify-between items-start'>
         <div className='w-full md:w-1/3 mb-8 md:mb-0'>
-            <img src={assets.logo_dark} alt="" />
+            <img src={assets.logo} alt="" />
             <p className='text-gray-400 mt-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
         </div>
         <div className='w-full md:w-1/5 mb-8 md:mb-0'>
